3e5cc0b4244fbb6211ad2548146fe5d75636bd1f
fernandodanielmaqueda <fmaqueda@frba.utn.edu.ar>
2022-04-07 10:35:24 -0300

    Resolucion TP1

--- src/TP1.hs
+++ src/TP1.hs
@@ -1,10 +1,20 @@
 module TP1 where
 
 esMes :: Int -> Bool
-esMes = undefined
+esMes mes = (mes >= 1) && (mes <= 12)
 
 hayCambioDeEstacion :: Int -> Bool
-hayCambioDeEstacion = undefined
+hayCambioDeEstacion mes = ((mod mes 3) == 0) && (esMes mes)
 
 estacion :: Int -> String
-estacion = undefined
+estacion mes
+ | esMes mes = estaciones mes
+
+estaciones :: Int -> String
+estaciones mes
+ | ((mes == 1) || (mes == 2)) = "verano"
+ | ((mes == 4) || (mes == 5)) = "otonio"
+ | ((mes == 7) || (mes == 8)) = "invierno"
+ | ((mes == 10) || (mes == 11)) = "primavera"
+ | (mes == 12) = "primavera/verano"
+ | otherwise = (estaciones (mes - 1)) ++ "/" ++ ((estaciones (mes + 1)))
