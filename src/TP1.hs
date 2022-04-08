5c9c496504b9daf62cfe4e59719b3940437725fa
fernandodanielmaqueda <fmaqueda@frba.utn.edu.ar>
2022-04-07 23:38:25 -0300

    Mejoras en funcion estacion

--- src/TP1.hs
+++ src/TP1.hs
@@ -8,13 +8,18 @@ hayCambioDeEstacion mes = ((mod mes 3) == 0) && (esMes mes)
 
 estacion :: Int -> String
 estacion mes
- | esMes mes = estaciones mes
-
-estaciones :: Int -> String
-estaciones mes
  | ((mes == 1) || (mes == 2)) = "verano"
  | ((mes == 4) || (mes == 5)) = "otonio"
  | ((mes == 7) || (mes == 8)) = "invierno"
  | ((mes == 10) || (mes == 11)) = "primavera"
- | (mes == 12) = "primavera/verano"
- | otherwise = (estaciones (mes - 1)) ++ "/" ++ ((estaciones (mes + 1)))
+ | (hayCambioDeEstacion mes) = (estacionMesAnterior mes) ++ "/" ++ (estacionMesSiguiente mes)
+
+estacionMesAnterior :: Int -> String
+estacionMesAnterior mes
+ | (mes == 1) = estacion 12
+ | (esMes mes) = estacion (mes - 1)
+
+estacionMesSiguiente :: Int -> String
+estacionMesSiguiente mes
+ | (mes == 12) = estacion 1
+ | (esMes mes) = estacion (mes + 1)
