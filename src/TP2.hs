59bf17890c9cc935fd5a6cea33bb6ea181e90b31
fernandodanielmaqueda <fmaqueda@frba.utn.edu.ar>
2022-04-14 03:46:36 -0300

    Resolución TP2

--- src/TP2.hs
+++ src/TP2.hs
@@ -10,7 +10,28 @@ data Celular = Celular {
   proveedor :: String
 } deriving (Show, Eq)
 
+prueba :: Celular
+prueba = Celular {
+  linea = ("011", "1234-5678"),
+  saldo = 100,
+  proveedor = "Movistar"
+}
 ------------------------------------------
 
 promoRecarga :: Int -> Celular -> Celular
-promoRecarga = undefined
+promoRecarga montoPagado celular = (Celular (linea celular) ((promociones celular) (saldo celular) montoPagado) (proveedor celular))
+
+promociones :: Celular -> (Int -> Int -> Int)
+promociones celular
+ | ((proveedor celular) == "Movistar") && ((fst (linea celular)) == "011") = promoMovistar
+ | ((proveedor celular) == "Personal") = promoPersonal
+ | otherwise = sinPromo
+
+promoMovistar :: Int -> Int -> Int
+promoMovistar saldoActual montoPagado = saldoActual + 3 * montoPagado
+
+promoPersonal :: Int -> Int -> Int
+promoPersonal saldoActual montoPagado = saldoActual + montoPagado + (min montoPagado 100)
+
+sinPromo :: Int -> Int -> Int
+sinPromo saldoActual montoPagado = saldoActual + montoPagado
