b3a1ca83844c3f77a7e5ac9666001fe0e8c14826
fernandodanielmaqueda <fmaqueda@frba.utn.edu.ar>
2022-05-06 20:10:57 -0300

    Resolución TP4

--- src/TP4.hs
+++ src/TP4.hs
@@ -2,16 +2,18 @@ module TP4 where
 import ContenidoAudiovisual
 
 tieneHashtag :: String -> Video -> Bool
-tieneHashtag = undefined
+tieneHashtag hashtag video = elem hashtag (hashtags video)
 
 minutosTotalesConHashtag :: String -> Playlist -> Int
-minutosTotalesConHashtag = undefined
+minutosTotalesConHashtag hashtag playlist = (foldl (+) 0.map (\video -> minutos video).filter (tieneHashtag hashtag)) (videos playlist)
 
 estanRelacionados :: Video -> Video -> Bool
-estanRelacionados = undefined
+estanRelacionados video1 video2 = (any (== True).map (\hashtag -> any (== hashtag) (hashtags video1))) (hashtags video2)
 
 recomendable :: Video -> Playlist -> Bool
-recomendable = undefined
+recomendable videoNuevo playlist = ((>= cantidad).length.filter (\videoPlaylist -> estanRelacionados videoNuevo videoPlaylist)) (videos playlist)
+    where
+        cantidad = 2
 
 agregarVideosRecomendados :: [Video] -> Playlist -> Playlist
-agregarVideosRecomendados = undefined
+agregarVideosRecomendados videosNuevos playlist = foldr (\videoNuevo playlist -> agregarAPlaylistSoloSi recomendable videoNuevo playlist) playlist videosNuevos
